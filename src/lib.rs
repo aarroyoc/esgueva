fbe1f51e1c948edf7b38b69bb4d6e33dd2010583
Adrián Arroyo Calle <adrian.arroyocalle@telefonica.com>
2022-09-15 09:15:24 +0200

    More test

--- src/lib.rs
+++ src/lib.rs
@@ -160,3 +160,15 @@ fn unify_str_4() {
     assert_eq!(Some(substitutions), result);    
 }
 
+#[test]
+fn unify_str_5() {
+    let left = Term::Str("f".to_string(), vec![Term::Var("X".to_string()), Term::Var("Y".to_string()), Term::Var("Y".to_string())]);
+    let right = Term::Str("f".to_string(), vec![Term::Var("Y".to_string()), Term::Var("X".to_string()), Term::Var("Y".to_string())]);
+    let result = unify(left, right);
+    let substitutions = HashMap::from([
+	("X".to_string(), Term::Var("Y".to_string())),
+	("Y".to_string(), Term::Var("Y".to_string())),
+    ]);
+    assert_eq!(Some(substitutions), result);    
+}
+
